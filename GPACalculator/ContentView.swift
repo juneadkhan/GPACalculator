d4681282aed751984114024916393f07bec5481d
Junead Khan <junead@live.unc.edu>
2020-08-23 13:24:58 +0100

    Initial Commit

---
+++ GPACalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GPACalculator
+//
+//  Created by Junead Khan on 23/08/2020.
+//  Copyright © 2020 Junead Khan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
